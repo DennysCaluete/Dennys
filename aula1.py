bff0d5ba53b16e8cfef8ec26e4bcac88ec0787cc
DennysCaluete <dennyspbrc@gmail.com>
2024-05-16 19:56:05 -0300

    primeiro commit

---
+++ aula1.py
@@ -0,0 +1,8 @@
+def ePar(numero):
+        if(numero%2) == 0:
+            print("{} é par".format(numero))
+        else:
+              print("{} é impar".format(numero))
+
+if __name__ == "__main__":
+    ePar(int(input("Qual o numero: ")))
